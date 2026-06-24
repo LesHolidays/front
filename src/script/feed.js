284ac8c2ae53f65bc7fcf3aef8730cb7a07de236
CorentinAT <corentin.al2004@gmail.com>
2026-06-24 18:24:44 +0200

    gestion commentaires

--- src/script/feed.js
+++ src/script/feed.js
@@ -1,6 +1,77 @@
 import { apiUrl } from "../utils/apiUrl.js";
+import { jwtDecode } from "https://esm.sh/jwt-decode";
 
 const activatedUsers = [];
+const commentariesElement = document.getElementById("commentaries");
+
+async function deleteCommentary(commentaryId) {
+  const response = await fetch(
+    apiUrl + "/commentaries?commentaryId=" + commentaryId,
+    {
+      method: "DELETE",
+      headers: {
+        authorization: "Bearer " + localStorage.getItem("access_token"),
+      },
+    },
+  );
+}
+
+async function getCommentaries(postId) {
+  const response = await fetch(apiUrl + "/commentaries?postId=" + postId, {
+    method: "GET",
+    headers: {
+      authorization: "Bearer " + localStorage.getItem("access_token"),
+    },
+  });
+  const commentaries = await response.json();
+
+  if (!commentaries || commentaries.length === 0) {
+    commentariesElement.textContent = "Aucun commentaire pour le moment";
+  } else {
+    commentariesElement.textContent = "";
+  }
+  for (let commentary of commentaries) {
+    const commentaryElement = document.createElement("div");
+    const nameElement = document.createElement("p");
+    const messageElement = document.createElement("p");
+    nameElement.textContent =
+      commentary.first_name + " " + commentary.last_name[0] + ".";
+    messageElement.textContent = commentary.commentary;
+
+    commentaryElement.append(nameElement, messageElement);
+
+    if (
+      commentary.user_id == jwtDecode(localStorage.getItem("access_token")).sub
+    ) {
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Supprimer";
+
+      commentaryElement.appendChild(deleteButton);
+
+      deleteButton.addEventListener("click", () => {
+        deleteCommentary(commentary.commentary_id);
+        commentaryElement.remove();
+        if (commentaries.length - 1 === 0) {
+          commentariesElement.textContent = "Aucun commentaire pour le moment";
+        }
+      });
+    }
+    commentariesElement.appendChild(commentaryElement);
+  }
+}
+
+async function addCommentary(postId, form) {
+  const formData = new FormData(form);
+  formData.append("postId", postId);
+
+  const response = await fetch(apiUrl + "/commentaries", {
+    method: "POST",
+    headers: {
+      authorization: "Bearer " + localStorage.getItem("access_token"),
+    },
+    body: formData,
+  });
+}
 
 async function submitVote(votedUserId, postId) {
   const formData = new FormData();
@@ -41,9 +112,7 @@ async function getPosts() {
     }
     const submitButton = document.createElement("button");
     submitButton.textContent = "Voter";
-    postElement.appendChild(imageElement);
-    postElement.appendChild(selectElement);
-    postElement.appendChild(submitButton);
+    postElement.append(imageElement, selectElement, submitButton);
     listElement.appendChild(postElement);
 
     submitButton.addEventListener("click", async () => {
@@ -56,7 +125,36 @@ async function getPosts() {
         const answerElement = document.createElement("div");
         answerElement.textContent = answer.creator;
         selectElement.replaceWith(answerElement);
-        submitButton.remove();
+
+        const commentaryForm = document.createElement("form");
+
+        const commentaryInput = document.createElement("input");
+        commentaryInput.type = "text";
+        commentaryInput.name = "message";
+        commentaryInput.placeholder = "Ajouter un commentaire";
+
+        const addCommentaryButton = document.createElement("input");
+        addCommentaryButton.value = "Envoyer";
+        addCommentaryButton.type = "submit";
+
+        commentaryForm.append(commentaryInput, addCommentaryButton);
+
+        submitButton.replaceWith(commentaryForm);
+
+        const commentariesButton = document.createElement("button");
+        commentariesButton.textContent = "Voir les commentaires";
+        postElement.appendChild(commentariesButton);
+
+        commentaryForm.addEventListener("submit", (e) => {
+          e.preventDefault();
+          addCommentary(post.post_id, e.target);
+          commentaryInput.value = "";
+        });
+
+        commentariesButton.addEventListener("click", () => {
+          commentariesElement.showModal();
+          getCommentaries(post.post_id);
+        });
       }
     });
   }
