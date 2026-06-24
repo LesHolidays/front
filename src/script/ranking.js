d6c38b4d5e12fc9acffe23ce28a1377bf63a87b7
CorentinAT <corentin.al2004@gmail.com>
2026-06-24 14:03:42 +0200

    ranking

---
+++ src/script/ranking.js
@@ -0,0 +1,25 @@
+import { apiUrl } from "../utils/apiUrl.js";
+
+async function getRanking() {
+  const response = await fetch(apiUrl + "/ranking");
+  const ranking = await response.json();
+
+  const tableElement = document.getElementById("ranking");
+
+  for (let userIndex in ranking) {
+    const rowElement = document.createElement("tr");
+    const rankingElement = document.createElement("td");
+    const nameElement = document.createElement("td");
+    const pointsElement = document.createElement("td");
+    nameElement.textContent =
+      ranking[userIndex].first_name + " " + ranking[userIndex].last_name;
+    rankingElement.textContent = +userIndex + 1;
+    pointsElement.textContent = ranking[userIndex].points + " points";
+    rowElement.append(rankingElement, nameElement, pointsElement);
+    tableElement.appendChild(rowElement);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  getRanking();
+});
